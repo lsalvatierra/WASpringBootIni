d1eddfab08f1f8a4f3809f824e24c59d3c81f637
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-03-24 21:41:00 -0500

    commit inicial

---
+++ src/main/java/edu/pe/idat/WaSpringBootIniApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaSpringBootIniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaSpringBootIniApplication.class, args);
+	}
+
+}
